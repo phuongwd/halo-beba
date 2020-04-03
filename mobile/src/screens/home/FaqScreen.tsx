46b0884f0cf79630226d55336e382caf6cffee1c
Miodrag Peric <misha@byteout.com>
2020-04-03 11:02:22 +0200

    ...

--- mobile/src/screens/home/FaqScreen.tsx
+++ mobile/src/screens/home/FaqScreen.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet, ViewStyle } from 'react-native';
+import { SafeAreaView, View, Text, Button, StyleSheet, ViewStyle, ScrollView } from 'react-native';
 import { NavigationStackProp, NavigationStackState, NavigationStackOptions } from 'react-navigation-stack';
 import { ThemeContextValue, ThemeConsumer } from '../../themes/ThemeContext';
+import { translate } from '../../translations/translate';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
+import { Typography, TypographyType } from '../../components/Typography';
+import { TextButton, TextButtonColor } from '../../components/TextButton';
+import { ListCard, ListCardMode } from './ListCard';
+import { listCardFaqYourChildDummyData, listCardFaqPerAgeDummyData, listCardFaqMamaDummyData } from '../../dummy-data/listCardDummyData';
 
 export interface FaqScreenParams {
 
@@ -31,17 +37,60 @@ export class FaqScreen extends React.Component<Props, object> {
         }
     }
 
+    private gotoBack() {
+        this.props.navigation.goBack();
+    }
+
     public render() {
         return (
             <ThemeConsumer>
             {(themeContext:ThemeContextValue) => (
-                <SafeAreaView style={ [styles.container, themeContext.theme.contentContainer] }>
-                    <View style={ {backgroundColor:'white', flex:1, flexDirection:'column', justifyContent:'flex-start', alignItems:'stretch'} }>
-                        <Text style={ {fontSize:20, textAlign:'left'} }>
-                            FaqScreen
-                        </Text>
-                    </View>
-                </SafeAreaView>
+                <ScrollView
+                    style={{backgroundColor:themeContext.theme.screenContainer?.backgroundColor}}
+                    contentContainerStyle={ [[styles.container], {padding:themeContext.theme.screenContainer?.padding}] }
+                >
+                    {/* GO BACK */}
+                    <TextButton style={{padding:0}} icon="chevron-left" iconStyle={{color:'#AA40BF'}} textStyle={{fontSize:scale(16)}} color={TextButtonColor.purple} onPress={ () => {this.gotoBack()} }>
+                        {translate('buttonBack')}
+                    </TextButton>
+
+                    <View style={{height:scale(15)}} />
+
+                    {/* TITLE */}
+                    <Typography type={TypographyType.headingPrimary}>
+                        { translate('faq') }
+                    </Typography>
+
+                    {/* LIST CARD: Your child */}
+                    <ListCard
+                        mode={ ListCardMode.simpleList }
+                        title={ listCardFaqYourChildDummyData.title }
+                        items={ listCardFaqYourChildDummyData.items }
+                        onItemPress={(item) => {console.warn(item.id)}}
+                    />
+
+                    <View style={{height:scale(20)}} />
+
+                    {/* LIST CARD: Per age */}
+                    <ListCard
+                        mode={ ListCardMode.simpleList }
+                        title={ listCardFaqPerAgeDummyData.title }
+                        items={ listCardFaqPerAgeDummyData.items }
+                        onItemPress={(item) => {console.warn(item.id)}}
+                    />
+
+                    <View style={{height:scale(20)}} />
+
+                    {/* LIST CARD: mama */}
+                    <ListCard
+                        mode={ ListCardMode.simpleList }
+                        title={ listCardFaqMamaDummyData.title }
+                        items={ listCardFaqMamaDummyData.items }
+                        onItemPress={(item) => {console.warn(item.id)}}
+                    />
+
+                    <View style={{height:scale(20)}} />
+                </ScrollView>
             )}
             </ThemeConsumer>
         );
@@ -55,6 +104,6 @@ export interface FaqScreenStyles {
 
 const styles = StyleSheet.create<FaqScreenStyles>({
     container: {
-        flex: 1,
+        
     },
 });
