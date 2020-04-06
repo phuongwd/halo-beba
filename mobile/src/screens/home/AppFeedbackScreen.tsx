633f86b0398ef5614caa2ad963880a20aa422d78
Miodrag Peric <misha@byteout.com>
2020-04-06 16:26:27 +0200

    Finished AppFeedbackScreen

--- mobile/src/screens/home/AppFeedbackScreen.tsx
+++ mobile/src/screens/home/AppFeedbackScreen.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { SafeAreaView, View, Text, Button, StyleSheet, ViewStyle, ScrollView } from 'react-native';
 import { NavigationStackProp, NavigationStackState, NavigationStackOptions } from 'react-navigation-stack';
 import { ThemeContextValue, ThemeConsumer } from '../../themes/ThemeContext';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { translate } from '../../translations/translate';
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import { Typography, TypographyType } from '../../components/Typography';
 import { TextButton, TextButtonColor } from '../../components/TextButton';
 import { RoundedTextArea } from '../../components/RoundedTextArea';
+import { RoundedButton, RoundedButtonType } from '../../components/RoundedButton';
 
 export interface AppFeedbackScreenParams {
 
@@ -50,38 +52,83 @@ export class AppFeedbackScreen extends React.Component<Props, State> {
         return (
             <ThemeConsumer>
             {(themeContext:ThemeContextValue) => (
-                <ScrollView
+                <KeyboardAwareScrollView
                     style={{backgroundColor:themeContext.theme.screenContainer?.backgroundColor}}
-                    contentContainerStyle={ [styles.container] }
+                    contentContainerStyle={ [styles.container, {alignItems:'stretch', padding:themeContext.theme.screenContainer?.padding}] }
                 >
-                    <View style={{alignItems:'stretch', padding:themeContext.theme.screenContainer?.padding}}>
-                        {/* GO BACK */}
-                        <TextButton style={{padding:0}} icon="chevron-left" iconStyle={{color:'#AA40BF'}} textStyle={{fontSize:scale(16)}} color={TextButtonColor.purple} onPress={ () => {this.gotoBack()} }>
-                            {translate('buttonBack')}
-                        </TextButton>
+                    {/* GO BACK */}
+                    <TextButton style={{padding:0}} icon="chevron-left" iconStyle={{color:'#AA40BF'}} textStyle={{fontSize:scale(16)}} color={TextButtonColor.purple} onPress={ () => {this.gotoBack()} }>
+                        {translate('buttonBack')}
+                    </TextButton>
 
-                        <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingNormal}} />
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingNormal}} />
 
-                        {/* TITLE */}
-                        <Typography type={TypographyType.headingPrimary}>
-                            { translate('appFeedbackTitle') }
-                        </Typography>
+                    {/* TITLE */}
+                    <Typography type={TypographyType.headingPrimary}>
+                        { translate('appFeedbackTitle') }
+                    </Typography>
 
-                        {/* SUB TITLE */}
-                        <Typography type={TypographyType.bodyRegular}>
-                            { translate('appFeedbackSubTitle') }
-                        </Typography>
+                    {/* SUB TITLE */}
+                    <Typography type={TypographyType.bodyRegular}>
+                        { translate('appFeedbackSubTitle') }
+                    </Typography>
 
-                        <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
 
-                        {/* QUESTION 1 */}
-                        <Typography type={TypographyType.headingSecondary}>
-                            { translate('appFeedbackQuestion1') }
-                        </Typography>
+                    {/* QUESTION 1 */}
+                    <Typography type={TypographyType.bodyRegular} style={{fontWeight:'bold', fontSize:moderateScale(20), marginBottom:scale(10)}}>
+                        { translate('appFeedbackQuestion1') }
+                    </Typography>
 
-                        <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
-                    </View>
-                </ScrollView>
+                    <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
+
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+
+                    {/* QUESTION 2 */}
+                    <Typography type={TypographyType.bodyRegular} style={{fontWeight:'bold', fontSize:moderateScale(20), marginBottom:scale(10)}}>
+                        { translate('appFeedbackQuestion2') }
+                    </Typography>
+
+                    <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
+
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+
+                    {/* QUESTION 3 */}
+                    <Typography type={TypographyType.bodyRegular} style={{fontWeight:'bold', fontSize:moderateScale(20), marginBottom:scale(10)}}>
+                        { translate('appFeedbackQuestion3') }
+                    </Typography>
+
+                    <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
+
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+
+                    {/* QUESTION 4 */}
+                    <Typography type={TypographyType.bodyRegular} style={{fontWeight:'bold', fontSize:moderateScale(20), marginBottom:scale(10)}}>
+                        { translate('appFeedbackQuestion4') }
+                    </Typography>
+
+                    <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
+
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+
+                    {/* QUESTION 5 */}
+                    <Typography type={TypographyType.bodyRegular} style={{fontWeight:'bold', fontSize:moderateScale(20), marginBottom:scale(10)}}>
+                        { translate('appFeedbackQuestion5') }
+                    </Typography>
+
+                    <RoundedTextArea placeholder={ translate('fieldLabelComment') } style={{backgroundColor:'white'}}></RoundedTextArea>
+
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+
+                    {/* SUBMIT BUTTON */}
+                    <RoundedButton
+                        text = { translate('appFeedbackButtonSend') }
+                        type = { RoundedButtonType.purple }
+                        onPress={ () => {} }
+                    />
+                    
+                    <View style={{height:themeContext.theme.variables?.sizes.verticalPaddingLarge}} />
+                </KeyboardAwareScrollView>
             )}
             </ThemeConsumer>
         );
