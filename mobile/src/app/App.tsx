d13d0496753bb079a91370e082f290fce8fecbc0
Miodrag Peric <misha@byteout.com>
2020-04-22 13:02:08 +0200

    ...

--- mobile/src/app/App.tsx
+++ mobile/src/app/App.tsx
@@ -49,10 +49,6 @@ export class App extends React.Component<object> {
     private configureGoogleSignin() {
         GoogleSignin.configure({
             scopes: ['https://www.googleapis.com/auth/drive.file'],
-            // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-            // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-            // accountName: '', // [Android] specifies an account name on the device that should be used
-            // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
         });
     }
 
