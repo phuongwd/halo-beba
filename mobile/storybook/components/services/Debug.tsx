9d51bb4fe914a9862c4ef858dcb0c49f5c27d412
Miodrag Peric <misha@byteout.com>
2020-05-06 15:39:58 +0200

    Created debug storybook 🐞

---
+++ mobile/storybook/components/services/Debug.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import { ScaledSheet, moderateScale, scale } from "react-native-size-matters";
+import { Typography, TypographyType } from "../../../src/components/Typography";
+import { Button, Colors } from "react-native-paper";
+import { dataRealmStore } from '../../../src/stores';
+
+export class Debug extends React.Component {
+    private deleteAllOnboardingData() {
+        dataRealmStore.deleteVariable('userEmail');
+        dataRealmStore.deleteVariable('userIsLoggedIn');
+        dataRealmStore.deleteVariable('followGrowth');
+        dataRealmStore.deleteVariable('followDoctorVisits');
+        dataRealmStore.deleteVariable('followDevelopment');
+        dataRealmStore.deleteVariable('allowAnonymousUsage');
+        dataRealmStore.deleteVariable('userIsOnboarded');
+        dataRealmStore.deleteVariable('userEnteredChildData');
+        dataRealmStore.deleteVariable('userParentalRole');
+        dataRealmStore.deleteVariable('userName');
+    }
+
+    render() {
+        return (
+            <ScrollView contentContainerStyle={{ flex: 1, padding: 24, alignItems: 'center' }}>
+                <Typography type={TypographyType.headingSecondary}>
+                    Debug
+                </Typography>
+
+                <Button mode="contained" uppercase={false} onPress={ () => {this.deleteAllOnboardingData()} } color={Colors.blue700}>
+                    Delete all onboarding data
+                </Button>
+                <View style={{ height: scale(10) }} />
+            </ScrollView>
+        );
+    }
+}
--- mobile/storybook/components/services/Services.stories.tsx
+++ mobile/storybook/components/services/Services.stories.tsx
@@ -4,8 +4,13 @@ import { storiesOf } from '@storybook/react-native';
 import { Google } from "./Google";
 import { Facebook } from "./Facebook";
 import { RealmDemo } from './RealmDemo';
+import { Debug } from './Debug';
 
 storiesOf('Services', module)
+
+.add('debug', () => (
+    <Debug />
+))
 
 .add('google', () => (
     <Google />
