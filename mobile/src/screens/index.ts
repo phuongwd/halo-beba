1c41b0a1fd82d8a8b307b05621864d493dc53592
Miodrag Peric <misha@byteout.com>
2020-05-19 16:26:45 +0200

    Updated API getContent/getAllContent 📰

--- mobile/src/screens/index.ts
+++ mobile/src/screens/index.ts
@@ -1,10 +1,12 @@
 import { AboutScreen } from './AboutScreen';
 import { Drawer } from "./Drawer";
 import { StorybookScreen } from "./StorybookScreen";
+import { SyncingScreen } from "./SyncingScreen";
 import { TestScreen } from "./TestScreen";
 import { VideoScreen } from "./VideoScreen";
 
 export { StorybookScreen };
+export { SyncingScreen };
 export { Drawer };
 export { AboutScreen };
 export { TestScreen };
