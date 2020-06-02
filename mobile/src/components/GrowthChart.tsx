508c7ba7971cb6223356809432272a1b3b5f8846
MilosByteOut <milos.ciganovic@byteout.com>
2020-06-02 18:36:35 +0200

    responsive && tooltip

--- mobile/src/components/GrowthChart.tsx
+++ mobile/src/components/GrowthChart.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { VictoryArea, VictoryLabel, VictoryScatter, VictoryChart, VictoryAxis, VictoryTheme, VictoryLine } from "victory-native";
-import { Text, ViewStyle, StyleSheet, View } from 'react-native';
+import { VictoryArea, VictoryLabel, VictoryTooltip, VictoryScatter, VictoryChart, VictoryAxis, VictoryTheme, VictoryLine } from "victory-native";
+import { Text, Dimensions, ViewStyle, StyleSheet, LayoutChangeEvent, View } from 'react-native';
 import Svg from 'react-native-svg';
 
 
-export interface ChartData{
+export interface ChartData {
     x: number,
-    y: number, 
+    y: number,
 }
 
 export interface Props {
@@ -22,13 +22,47 @@ export interface Props {
     areaExceptionsChartData: ChartData[],
 }
 
-export class GrowthChart extends React.Component<Props> {
+export interface State {
+    orientation: 'portrait' | 'landscape';
+    width: number,
+    height: number,
+}
+
+export class GrowthChart extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props)
+        this.initState()
+    }
+
+    private initState() {
+        let windowWidth = Dimensions.get('window').width;
+        let windowHeight = Dimensions.get('window').height;
+
+        let state: State = {
+            orientation: windowWidth > windowHeight ? 'landscape' : 'portrait',
+            width: windowWidth,
+            height: windowHeight,
+        };
+
+        this.state = state;
+    }
+
+
+    private onLayout = (event: LayoutChangeEvent) => {
+        let layout = event.nativeEvent.layout;
+
+        this.setState({
+            width: layout.width,
+            height: layout.height,
+        })
+    }
 
     public render() {
 
-        // SET MAX AND MIN VALUES FOR RANGE 
-        const sortedXdata = this.props.dataX.sort((n1: number, n2: number) => n1 - n2);
-        const sortedYdata = this.props.dataY.sort((n1: number, n2: number) => n1 - n2);
+        // SET MAX AND MIN VALUES TO CREATE RANGE FOR X and Y => (X[0] === minX, x[x.length] === max X)
+        const sortedXdata: number[] = this.props.dataX.sort((n1: number, n2: number) => n1 - n2);
+        const sortedYdata: number[] = this.props.dataY.sort((n1: number, n2: number) => n1 - n2);
 
         const minDomain = {
             x: sortedXdata[0] - sortedXdata[0],
@@ -41,7 +75,7 @@ export class GrowthChart extends React.Component<Props> {
         }
 
         return (
-            <View style={styles.container}>
+            <View style={styles.container} onLayout={this.onLayout}>
                 <Text>{this.props.title}</Text>
                 <Svg style={{ marginLeft: -10 }} >
                     <VictoryChart
@@ -49,15 +83,15 @@ export class GrowthChart extends React.Component<Props> {
                         minDomain={minDomain}
                         maxDomain={maxDomain}
                         domainPadding={20}
-                        width={370}
-                        height={350}
+                        width={this.state.width}
+                        height={this.state.height}
                     >
                         {/* ********* AXIS HORIZONTAL ********* */}
                         <VictoryAxis
                             tickFormat={this.props.dataX}
                             style={victoryStyles.VictoryAxis}
                             label={this.props.labelXText}
-                            axisLabelComponent={<VictoryLabel x={this.props.labelXPosition || 305} />}
+                            axisLabelComponent={<VictoryLabel x={this.props.labelXPosition || this.state.width - 20} />}
                         />
 
                         {/* ********* AXIS VERTICAL ********* */}
@@ -95,6 +129,7 @@ export class GrowthChart extends React.Component<Props> {
                             data={this.props.lineChartData}
                             size={9}
                             style={victoryStyles.VictoryScatter}
+                            labelComponent={<VictoryTooltip />}
                             labels={() => null}
                             events={[{
                                 target: "data",
@@ -105,16 +140,27 @@ export class GrowthChart extends React.Component<Props> {
                                                 target: "data",
                                                 mutation: (props) => {
                                                     const stroke = props.style && props.style.stroke;
-                                                    console.log(props.style.stroke, 'style')
                                                     return stroke === "orange" ? null : { style: { stroke: "orange", strokeWidth: 4, fill: 'white' } };
                                                 }
                                             },
                                             {
                                                 target: "labels",
                                                 mutation: (props) => {
-                                                    console.log(props, 'props')
-                                                    return typeof props.text === "string" ?
-                                                        null : { text: props.datum.y + " " + this.props.labelYText + ' / ' + props.datum.x + ' ' + this.props.labelXText };
+                                                    return typeof props.active === "boolean" ?
+                                                        null : { active:  true, text: props.datum.y + ' cm' + ' / ' + props.datum.x + ' kg ' };
+                                                }
+                                            },
+
+                                        ]
+                                    },
+                                    onPressOut: () => {
+                                        return [
+                                            {
+                                                target: "labels",
+                                                mutation: (props) => {
+                                                    console.log(props.active, 'active')
+                                                    return { active: props.active, text: props.datum.y + " " + this.props.labelYText + ' / ' + props.datum.x + ' ' + this.props.labelXText }
+
                                                 }
                                             },
 
@@ -174,7 +220,8 @@ const victoryStyles: VictoryStyles = {
         data: { stroke: "#0C66FF", strokeWidth: 9, strokeLinecap: "round", }
     },
     VictoryScatter: {
-        data: { fill: "white", stroke: 'grey', strokeWidth: 3 }
+        data: { fill: "white", stroke: 'grey', strokeWidth: 3 },
+        labels: { fill: "red" }
     },
     VictoryArea: {
         data: { fill: "silver" }
