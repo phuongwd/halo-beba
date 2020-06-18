dca10f79b9484f18d2dafe812c2cd9908adb16b1
Miodrag Peric <misha@byteout.com>
2020-06-18 15:56:31 +0200

    Added hard coded development info for serbian

--- mobile/src/translationsData/en.ts
+++ mobile/src/translationsData/en.ts
@@ -1,3 +1,332 @@
 export const en = {
-    foo: {kar:'mar en'},
+    developmentPeriods: [
+        {
+            period: '1. mesec',
+            name: 'Razvoj u 1. mesecu',
+            moreAboutPeriodArticleIdMale: 3466,
+            moreAboutPeriodArticleIdFemale: 3461,
+            description: `
+<p>
+    Ovo je period upoznavanja
+</p>
+
+<ul>
+    <li>Beba se navikava na ovaj čudan, veliki, novi svet</li>
+    <li>Maženje, spavanje i hranjenje su sve što joj je potrebno tokom prvog meseca života. </li>
+    <li>Vreme sa vama, vaš osmeh, glas i toplina najbolji su za razvoj vaše bebe</li>
+</ul>
+
+<p>
+    Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta.
+</p>
+`,
+            warningText: `
+<p>
+    Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Uopšte ne reaguje na glasne zvuke</li>
+    <li>Ne može da odigne glavu i oslobodi disajne puteve kada leži na stomaku.</li>
+</ul>
+`,
+            predefinedTagId: 43,
+        },
+
+        {
+            period: '2 meseca',
+            name: 'Razvoj u 2. mesecu',
+            moreAboutPeriodArticleIdMale: 3476,
+            moreAboutPeriodArticleIdFemale: 3471,
+            description: `
+<p>
+    Ovo je period međusobnog usklađivanja
+</p>
+
+<ul>
+    <li>Roditelji i beba se polako međusobno upoznaju i uigravaju </li>
+    <li>U ovom periodu beba će biti sve aktivnija i pokretljivija</li>
+    <li>Karakteristično je da u ovom mesecu beba sve više plače – tako komunicira sa vama </li>
+</ul>
+
+<p>
+    Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Uopšte ne reaguje na glasne zvuke</li>
+    <li>Ne prati stvari koje se pomeraju</li>
+    <li>Ne prinosi ruke ustima</li>
+    <li>Ne može da odigne glavu od podloge kada leži na stomaku</li>
+</ul>
+`,
+            predefinedTagId: 44,
+        },
+
+        {
+            period: '3-4 meseca',
+            name: 'Razvoj u 3. i  4. mesecu',
+            moreAboutPeriodArticleIdMale: 3486,
+            moreAboutPeriodArticleIdFemale: 3481,
+            description: `
+<p>
+Ovo je period raznovrsnih iskustava
+</p>
+
+<ul>
+<li>Beba sve češće komunicira osmehom</li>
+<li>Beba želi da istražuje svet – prevrće se i poseže za predmetima</li>
+<li>Počinje da prepoznaje glasove onih koji brinu o njoj, a i dalje je najviše interesuju lica</li>
+</ul>
+            
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+<li>Ne prati stvari koje se pomeraju</li>
+<li>Ne reaguje osmehom na ljudksi lik</li>
+<li>Ne drži (ne kontroliše) glavu stabilno</li>
+<li>Ne guče i ne pravi zvuke</li>
+<li>Ne prinosi stvari ustima.</li>
+</ul>
+`,
+            predefinedTagId: 45,
+        },
+
+        {
+            period: '5-6 meseci',
+            name: 'Razvoj u 5. i 6. mesecu',
+            moreAboutPeriodArticleIdMale: 3496,
+            moreAboutPeriodArticleIdFemale: 3491,
+            description: `
+<p>
+Ovo je period intenzivnog istraživanja
+</p>
+
+<ul>
+<li>Beba može da sedi, okreće se i sama podiže i drži glavicu</li>
+<li>Posegnuće za zanimljivim predmetima iz okoline</li>
+<li>Raspoznaje poznate osobe i počinje da komunicira gukanjem</li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Ne okreće se ni na jednu stranu</li>
+    <li>Ne drži glavu kada leži na stomaku ili se postavi u poduprt sedeći položaj a kada se drži uspravno previše je stegnuto (napeto) ili opušteno (mlitavo)</li>
+    <li>Ne istražuje objekte ni pogledom, ni rukama, ni stavljajući ih u usta ili ne približava ni ruke ni noge ka sredini tela ili ka ustima. </li>
+    <li>Ne poseže čak ni za objektima koji su mu dostupni</li>
+    <li>Ne izgovara nijedan od samoglasnika</li>
+    <li>Ne osmehuje se drugima ili ne pokazuje interesovanje za ljude oko sebe</li>
+</ul>
+`,
+            predefinedTagId: 46,
+        },
+
+        {
+            period: '7-9 meseci',
+            name: 'Razvoj od 7. do 9. meseca',
+            moreAboutPeriodArticleIdMale: 3511,
+            moreAboutPeriodArticleIdFemale: 3506,
+            description: `
+<p>
+Ovo je vreme za nova otkrića
+</p>
+
+<ul>
+    <li>Samostalno sedenje pruža bebi novu perspektivu sveta, a puzanje i prevrtanje nove mogućnosti za saznanje</li>
+    <li>Beba komunicira brbljanjem, a možda je već vreme i za prvu reč</li>
+    <li>Sve je više privržena poznatim osobama, a sve se češće se plaši stranaca</li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Ne može, uz pridržavanje, da održi na nogama svoju težinu</li>
+    <li>Ne sedi uz podršku</li>
+    <li>Ne počinje da brblja i udvaja glasove: ba-ba, ma-ma, da-da</li>
+    <li>Ne reaguje na sopstveno ime</li>
+    <li>Ne prepoznaje ukućane</li>
+    <li>Ne gelda u pravcu koji mu pokazujete</li>
+    <li>Ne prebacuje igračke iz ruke u ruku</li>
+</ul>
+`,
+            predefinedTagId: 47,
+        },
+
+        {
+            period: '10-12 meseci',
+            name: 'Razvoj od 10. do 12. meseca',
+            moreAboutPeriodArticleIdMale: 3521,
+            moreAboutPeriodArticleIdFemale: 3516,
+            description: `
+<p>
+Ovo je vreme ustaljivanja navika i doslednosti
+</p>
+
+<ul>
+    <li>Vaša beba sve više želi da komunicira – pokazuje, klima, odmahuje...</li>
+    <li>Ovo je period kada će se najverovatnije javiti i prva reč i prvi korak</li>
+    <li>Beba uči sve vreme – a najviše uči kroz igru sa vama</li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Ne puzi</li>
+    <li>Ne stoji uz pridržavanje</li>
+    <li>Ne traži predmete koje ste sakrili</li>
+    <li>Ne pokazuje predmete</li>
+    <li>Nije naučilo uobičajene gestove: mahanje uz pa-pa, odmahivanje glavom kada negoduje. </li>
+</ul>
+`,
+            predefinedTagId: 48,
+        },
+
+        {
+            period: '13-18 meseci',
+            name: 'Razvoj od 13. do 18. meseca',
+            moreAboutPeriodArticleIdMale: 3531,
+            moreAboutPeriodArticleIdFemale: 3526,
+            description: `
+<p>
+Ovo je period prvog koraka i prve reči
+</p>
+
+<ul>
+    <li>I krupna motorika i fini pokreti se intenzivno razvijaju – vaše dete pravi prvi korak, ali i sve je bolje u upotrebi prstića</li>
+    <li>Mogućnost kretanja povećaće radoznalost vašeg deteta  i želju za istraživanjem, ali će se vaše ono  i dalje najsigurnije osećati kada ste vi u blizini</li>
+    <li>Dete vas sve bolje razume, a tu su i prve reči pa je međusobna komunikacija sve bolja</li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta"
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Ne pokazuje interesovanje za interakcije i igre sa drugim osobama</li>
+    <li>Ne izgovara prve reči</li>
+    <li>Nije u stanju da razume kratke instrukcije (npr. “Gde je lopta?”)</li>
+    <li>Ne šara olovkom po papiru</li>
+    <li>Nakon što je pokazano, ne pokušava imitira stavljanje jedne kocke na drugu</li>
+    <li>Ne stoji samostalno</li>
+    <li>Ne pokušava da hoda bez podrške</li>
+</ul>
+`,
+            predefinedTagId: 49,
+        },
+
+        {
+            period: '19-24 meseca',
+            name: 'Razvoj od 19. do 24. meseca',
+            moreAboutPeriodArticleIdMale: 3541,
+            moreAboutPeriodArticleIdFemale: 3536,
+            description: `
+<p>
+Ovo je period osamostaljivanja
+</p>
+
+<ul>
+    <li>Očekujte nove pokrete-trčanje, penjanje... ali i nove emocije i nove reči</li>
+    <li>Igra je sve kreativnija i sve je zastupljenija “kao da” igra</li>
+    <li>Vaše dete osvaja samostalnost – samo se hrani ili oblači </li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta"
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Ne zna kako se koriste stvari za svakodnevu upotrebu</li>
+    <li>Ne imitira reči i aktivnosti odraslih</li>
+    <li>Ne prati jednostavne instrukcije</li>
+    <li>Ne koristi dve vezane reči</li>
+    <li>Ne hoda stabilno</li>
+    <li>Gubi sposobnosti koje je već steklo</li>
+</ul>
+`,
+            predefinedTagId: 50,
+        },
+
+        {
+            period: '25-36 meseci',
+            name: 'Razvoj od 25. do 36. meseca',
+            moreAboutPeriodArticleIdMale: 3551,
+            moreAboutPeriodArticleIdFemale: 3546,
+            description: `
+<p>
+Ovo je period kad mašta cveta
+</p>
+
+<ul>
+    <li>Dete osvaja samostalnost – može samo da jede, obuče se i svuče, pa čak i koristi nošu ili toalet</li>
+    <li>Kreće se sve spretnije, a sada i trči, skače, šuta, hvata, pa čak stoji i na jednoj nozi</li>
+    <li>Emocije su vrlo intenzivne i raznolike, a polako uči i reči kojima može da ih izrazi</li>
+</ul>
+
+<p>
+Popunite kratki upitnik za ovaj razvojni period, saznajte više kako da podstičete razvoj vašeg deteta"
+</p>
+`,
+            warningText: `
+<p>
+Pratite razvoj i obratite se pedijatru ili patronažnoj sestri ukoliko beba:
+</p>
+
+<ul>
+    <li>Često pada ili ima problem sa hodanjem po stepenicama</li>
+    <li>Nerazgovetno govori</li>
+    <li>Ne razume jednostavne naloge</li>
+    <li>Ne izgovara rečenice</li>
+    <li>Nema kontakt očima</li>
+    <li>Ne igra se igara pretvaranja – “kao da” igre</li>
+    <li>Ne želi da se igra sa drugom decom ili sa igračkama</li>
+    <li>Gubi sposobnosti i veštine koje je već steklo </li>
+</ul>
+`,
+            predefinedTagId: 51,
+        },
+    ],
 };
