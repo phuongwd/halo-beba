07dc03ba3ae5eeddf0705c068566c8ff487f6761
MilosByteOut <milos.ciganovic@byteout.com>
2020-06-30 09:29:08 +0200

    About screen imort fixe

--- mobile/src/stores/dataRealmConfig.ts
+++ mobile/src/stores/dataRealmConfig.ts
@@ -1,7 +1,7 @@
 import Realm from 'realm';
 import { ContentEntitySchema } from './ContentEntity';
 import { VariableEntitySchema } from './VariableEntity';
-import { BasicPagesEntitySchema as BasicPagesEntitySchema } from './BasicPageEntity';
+import { BasicPagesEntitySchema } from './BasicPageEntity';
 
 export const dataRealmConfig: Realm.Configuration = {
     // API: https://bit.ly/36WypWV
