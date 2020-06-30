b6be112b7e2b353152197e7daf88801428abeede
MilosByteOut <milos.ciganovic@byteout.com>
2020-06-30 12:02:28 +0200

    appConfig developer mode sync turned off

--- mobile/src/app/appConfig.ts
+++ mobile/src/app/appConfig.ts
@@ -19,6 +19,6 @@ export const appConfig = {
     // DEVELOPMENT
     // Set to true only during development
     showLog: true, 
-    preventSync: true,
+    preventSync: false,
     deleteRealmFilesBeforeOpen: false,
 };
